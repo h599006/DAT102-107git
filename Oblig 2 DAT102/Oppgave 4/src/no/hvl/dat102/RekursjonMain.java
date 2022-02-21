432bb1a5ff147d0b6424cc010d01d53a58d5e3b1
h599006 <89257467+h599006@users.noreply.github.com>
2022-02-21 12:20:13 +0100

    Update RekursjonMain.java

--- Oblig 2 DAT102/Oppgave 4/src/no/hvl/dat102/RekursjonMain.java
+++ Oblig 2 DAT102/Oppgave 4/src/no/hvl/dat102/RekursjonMain.java
@@ -20,7 +20,9 @@ public class RekursjonMain {
 		System.out.println("--------------------");
 		
 		//d)
-		System.out.println(Rekursjon.fibonacciUtenRekursjon(10));
+		for (int i = 0; i <= 10; i++) {
+			System.out.println(Rekursjon.fibonacciUtenRekursjon(i));
+		}
 		System.out.println("--------------------");
 		
 	}
