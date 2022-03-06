1a0ead978254240b611d431f7cb0070eb671b031
h599006 <89257467+h599006@users.noreply.github.com>
2022-03-06 12:42:18 +0100

    Add files via upload

---
+++ Oblig3 Oppgave1/src/no/hvl/dat102/SorteringVedInnsetting.java
@@ -0,0 +1,70 @@
+package no.hvl.dat102;
+
+public class SorteringVedInnsetting {
+	
+	public static <T extends Comparable<? super T>> void sorteringVedInnsetting(T[] a, int n) {
+		sorteringVedInssetting(a, 0, n - 1);
+	}
+
+	public static <T extends Comparable<? super T>> void sorteringVedInssetting(T[] a, int start, int slutt) {
+		
+		for (int i = start + 1; i <= slutt; i++) {
+			T temp = a[i];
+			int j = i - 1;
+			
+			while (j >= 0) { //Fjerner boolean fordi da er det en ting mindre å sjekke, å sjekke ting tar tid
+				if (temp.compareTo(a[j]) < 0) {
+					a[j + 1] = a[j];
+					j--;
+				} else {
+					break;
+				}
+			}
+			a[j + 1] = temp;
+		}
+	}
+	
+	
+	public static <T extends Comparable<? super T>> void sorteringVedInnsettingToTall(T[] a, int n) {
+		sorteringVedInssettingToTall(a, 0, n - 1);
+	}
+	
+	public static <T extends Comparable<? super T>> void sorteringVedInssettingToTall(T[] a, int start, int slutt) {
+	
+		for (int i = start + 1; i < slutt; i+=2) {
+			T temp = a[i];
+			T temp2 = a[i + 1];
+			int j = i - 1;
+			
+			if (i + 1 < slutt) {
+				i--;
+			}
+			if (temp.compareTo(temp2) < 0) {
+				T midlertidig = temp2;
+				temp2 = temp;
+				temp = midlertidig;
+			} 
+			
+			while (j >= 0) { 
+				if (temp.compareTo(a[j]) < 0) {
+					a[j + 2] = a[j];
+					j--;
+				} else {
+					break;
+				}	
+			}
+			
+			while (j >= 0) { 
+				if (temp2.compareTo(a[j]) < 0) {
+					a[j + 1] = a[j];
+					j--;
+				} else {
+					break;
+				}
+			}
+			a[j + 1] = temp;
+			
+		} 
+		
+	}
+}
